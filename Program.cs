ef60cc521f502eb4a37e3b46b40bdc6780a34bf6
HuyDo <huydo93.it@gmail.com>
2025-04-30 23:08:56 +0900

    firt commit to Develop

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Reflection;
+string strFormat = "Hello {0}, welcome to {1}!";
+string strName = "Huy";
+string strPlace = "CircleCI";
+
+Console.WriteLine(string.Format(strFormat, strName, strPlace));
 Console.ReadKey();
+
+
