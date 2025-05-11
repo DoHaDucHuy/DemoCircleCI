a0e7670db427545d7e4d27e791c5f825552fe498
HuyDo <huydo93.it@gmail.com>
2025-05-11 22:57:46 +0900

    add Sum() func

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Reflection;
-string strFormat = "Hello {0}, welcome to {1}!";
-string strName = "Huy";
-string strPlace = "CircleCI";
-
-Console.WriteLine(string.Format(strFormat, strName, strPlace));
+Console.WriteLine("Hello, World!");
+Console.Write("Input A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Sum: " + Sum(a, b));
 Console.ReadKey();
-
+static int Sum(int a, int b)
+{
+    return a + b;
+}
 
